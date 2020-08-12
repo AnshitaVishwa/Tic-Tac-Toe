822845487b2294d804f6ab03aa482787998cc1df
AnshitaVishwa <holaholafires@gmail.com>
2020-08-12 19:25:51 +0530

    board Display

--- v1.cpp
+++ v1.cpp
@@ -2,6 +2,33 @@
 using namespace std;
 
 class Tic_Tac_toe {
+private:
     int board[3][3];
-    
+public:
+    Tic_Tac_toe () {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                board[i][j] = -1;
+            }
+        }
+    }
+    void displayBoard () {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                if (board[i][j] == -1) {
+                    cout << "    ";
+                } else {
+                    cout << "   " << board[i][j] << "   ";
+                }
+                if(j != 2) cout << "|";
+            }
+            if (i != 2) cout << "\n-------------------------\n";
+        }
+    }
 };
+
+int main() {
+    Tic_Tac_toe t1;
+    t1.displayBoard();
+    return 0;
+}
